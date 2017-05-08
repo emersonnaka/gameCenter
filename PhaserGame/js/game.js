82c36f8ab9663e119bd3993001bf1715870ba4e3
emersonnaka <emerson.nakashima@gmail.com>
2017-05-08 12:01:08 -0300

    Mapa inicializado e plotado

---
+++ PhaserGame/js/game.js
@@ -0,0 +1,45 @@
+class Config {}
+Config.WIDTH = 1000
+Config.HEIGHT= 1000
+Config.DEBUG = false
+Config.ANTIALIAS = true
+Config.ASSETS = 'assets/'
+
+class Game extends Phaser.Game {
+    constructor() {
+        super(Config.WIDTH, Config.HEIGHT, Phaser.CANVAS, 'game-container', null, false, Config.ANTIALIAS)
+
+        this.state.add('Play', PlayState, false)
+        this.state.start('Play')
+    }
+}
+
+class PlayState extends Phaser.State {
+	preload() {
+		this.game.load.tilemap('level1', Config.ASSETS + 'phase1.json', null, Phaser.Tilemap.TILED_JSON)
+		this.game.load.image('tiles', Config.ASSETS + 'tiles/tiles.png')
+		this.game.load.image('background', Config.ASSETS + 'background.png')
+	}
+
+	create() {
+		this.game.physics.startSystem(Phaser.Physics.ARCADE)
+        this.game.stage.backgroundColor = '#000000'
+		
+		let bg = this.game.add.tileSprite(0, 0, Config.WIDTH, Config.HEIGHT, 'background')
+        bg.fixedToCamera = true
+
+        this.createMap()
+
+        let keys = this.game.input.keyboard.createCursorKeys()
+	}
+
+	createMap() {
+		this.map = this.game.add.tilemap('level1')
+        this.map.addTilesetImage('tiles')
+
+        this.mapLayer = this.map.createLayer('Tile Layer')
+        this.mapLayer.resizeWorld()
+	}
+}
+
+const GAME = new Game()
