363a8a86fc66d13f28be49c61fa29acfebaa332f
nelsonviiera <nelsonj@utfpr.edu.br>
2017-05-12 19:10:24 -0300

    mapa modificado e server adicionado

---
+++ WebServer/src/webserver/DAO.java
@@ -0,0 +1,133 @@
+package webserver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAO {
+
+    private static final String url = "jdbc:mysql://localhost:3306/";
+    private static final String dbName = "web";
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String userName = "root";
+    private static final String password = "root";
+
+    public static boolean addPlayer(int id, String nome, String email) {
+        Connection conexao = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName(driver).newInstance();
+            conexao = DriverManager.getConnection(url + dbName, userName, password);
+            Statement statement = conexao.createStatement();
+            String sql = "INSERT INTO Player(id, nome, email) VALUES (" + id + ",'" + nome + "','" + email + "')";
+
+            statement.execute(sql);
+            statement.close();
+            return true;
+
+        } catch (Exception ex) {
+            System.out.println("Erro : " + ex.getMessage());
+            return false;
+        }
+    }
+
+    public static boolean verificaPlayer(int id) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+        Connection conexao = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        Class.forName(driver).newInstance();
+        conexao = DriverManager.getConnection(url + dbName, userName, password);
+        pst = conexao.prepareStatement("select Player.id from player where Player.id = " + "" + id + "");
+        rs = pst.executeQuery();
+
+        if (rs != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public static boolean addTrophy(String name, int xp, String title, String description) {
+        Connection conexao = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName(driver).newInstance();
+            conexao = DriverManager.getConnection(url + dbName, userName, password);
+            Statement statement = conexao.createStatement();
+            String sql = "INSERT INTO Trophy(name, xp, title, description) VALUES ( '" + name + "'," + xp + ",'" + title + "','" + description + "')";
+
+            statement.execute(sql);
+            statement.close();
+            return true;
+
+        } catch (Exception ex) {
+            System.out.println("Erro : " + ex.getMessage());
+            return false;
+        }
+    }
+    
+    /*public static ArrayList<Trophy> listTrophy() throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException{
+        Connection conexao = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        
+        Class.forName(driver).newInstance();
+        conexao = DriverManager.getConnection(url + dbName, userName, password);
+        pst = conexao.prepareStatement("SELECT * from Trophy");
+        
+        
+        rs = pst.executeQuery();
+        while (rs.next()){
+            
+        }
+    }*/
+
+    public static String getPlayer() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        Connection conexao = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        Class.forName(driver).newInstance();
+        conexao = DriverManager.getConnection(url + dbName, userName, password);
+        pst = conexao.prepareStatement("select * from Player ");
+
+        rs = pst.executeQuery();
+        String aux = "";
+        while (rs.next()) {
+            aux = "{id:" + String.valueOf(rs.getInt("Player.id"));
+            aux += ", nome:" + rs.getString("nome");
+            aux += ", email:" + rs.getString("email") + "}";
+        }
+        return aux;
+    }
+
+    public static boolean atualizarUsuario(int id, String nome, String email) {
+        Connection conexao = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName(driver).newInstance();
+            conexao = DriverManager.getConnection(url + dbName, userName, password);
+            try (Statement statement = conexao.createStatement()) {
+                String sql = "UPDATE Player SET nome='" + nome + "', email='" + email + "' where Player.id = " + id + "";
+                statement.execute(sql);
+            }
+            return true;
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            System.out.println("Erro : " + ex.getMessage());
+            return false;
+        }
+    }
+}
