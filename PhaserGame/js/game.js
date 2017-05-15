26bd1023921981d5ec92eacca6d57b6ca0a8285b
emersonnaka <emerson.nakashima@gmail.com>
2017-05-15 17:39:17 -0300

    Renomeação das imagens e player na tela

--- PhaserGame/js/game.js
+++ PhaserGame/js/game.js
@@ -1,7 +1,7 @@
 class Config {}
-Config.WIDTH = 1024
-Config.HEIGHT= 1024
-Config.DEBUG = false
+Config.WIDTH = 800
+Config.HEIGHT= 400
+Config.DEBUG = true
 Config.ANTIALIAS = true
 Config.ASSETS = 'assets/'
 
@@ -20,6 +20,8 @@ class PlayState extends Phaser.State {
 		this.game.load.image('tiles', Config.ASSETS + 'tiles/tiles.png')
 		this.game.load.image('objects', Config.ASSETS + 'objects/objects.png')
 		this.game.load.image('background', Config.ASSETS + 'background.png')
+
+		this.game.load.spritesheet('character', Config.ASSETS + 'character/character.png', 75, 90)
 	}
 
 	create() {
@@ -29,7 +31,11 @@ class PlayState extends Phaser.State {
 		let bg = this.game.add.tileSprite(0, 0, Config.WIDTH, Config.HEIGHT, 'background')
         bg.fixedToCamera = true
 
+        this.keys = this.game.input.keyboard.createCursorKeys()
+        this.game.physics.arcade.gravity.y = 550
+
         this.createMap()
+        this.createPlayer()
 
         let keys = this.game.input.keyboard.createCursorKeys()
 	}
@@ -40,7 +46,28 @@ class PlayState extends Phaser.State {
         this.map.addTilesetImage('objects')
 
         this.mapLayer = this.map.createLayer('Tile Layer')
+
+        this.map.setCollisionBetween(1, 18, true, 'Tile Layer')
+        // this.map.setCollisionBetween(6, 8, true, 'Tile Layer')
+        // this.map.setCollisionBetween(10, 13, true, 'Tile Layer')
+        // this.map.setCollisionBetween(16, 18, true, 'Tile Layer')
         this.mapLayer.resizeWorld()
+	}
+
+	createPlayer() {
+		this.player = new Player(this.game, this.keys, 50, 100, 'character')
+		this.game.add.existing(this.player)
+		this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1)
+	}
+
+	update() {
+		this.game.physics.arcade.collide(this.player, this.mapLayer)
+	}
+
+	render() {
+		if(Config.DEBUG) {
+			this.game.debug.body(this.player)
+		}
 	}
 }
 
