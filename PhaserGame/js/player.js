7e00c996545ea40f1d5e2a7809851ae2e08f897e
emersonnaka <emerson.nakashima@gmail.com>
2017-05-31 10:07:12 -0300

    Ataca, mas não roda a animação inteira

--- PhaserGame/js/player.js
+++ PhaserGame/js/player.js
@@ -7,10 +7,12 @@ class Player extends Phaser.Sprite {
         this.body.setSize(38, 45, 5, 11)
         this.anchor.setTo(0.5, 0.5)
         this.lifes = 0
+        this.isAttack = false;
 
         this.animations.add('idle', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, true)
         this.animations.add('run', [10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 20, true)
         this.animations.add('jump', [20], 10, true)
+        this.animations.add('attack', [21, 22, 23, 24, 25, 26, 27, 28, 29, 30], 30, true)
 
 
         let jumpButton = this.game.input.keyboard.addKey(
@@ -18,7 +20,7 @@ class Player extends Phaser.Sprite {
         jumpButton.onDown.add(this.jump, this)
 
         let attackButton = this.game.input.keyboard.addKey(Phaser.Keyboard.CONTROL)
-        // attackButton.onDown.add(this.attack, this)
+        attackButton.onDown.add(this.attack, this)
     }
 
     update() {
@@ -42,6 +44,12 @@ class Player extends Phaser.Sprite {
         // No ar
         if (this.body.velocity.y != 0)
             this.animations.play('jump')
+        else {
+            if(this.isAttack) {
+                this.animations.play('attack')
+                this.isAttack = false;
+            }
+        }
 
         // Define o lado
         if (this.body.velocity.x > 0)
@@ -54,6 +62,10 @@ class Player extends Phaser.Sprite {
         if (this.body.onFloor()) {
             this.body.velocity.y = -350
         }
+    }
+
+    attack() {
+        this.isAttack = true;
     }
 
     subLife() {
